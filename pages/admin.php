2540c54ac578c84a2e30f3e8785776fac8bcad03
Bill Kehagias <b.keh97@hotmail.com>
2019-05-21 01:21:21 +0300

    1st commit, we are the very best programmers of the worlds
    Wryyyyyy

---
+++ pages/admin.php
@@ -0,0 +1,37 @@
+<?php 
+	include 'adminheader.php';
+?>
+					<article class="content three_quarter">
+						
+						<h2>Καλωσόρισες, <?php if($_SESSION['loggedin'] == "bkeh97"){echo "Supreme Administrator ";} echo($_SESSION['loggedin']); ?></h2>
+						<?php
+							if(isset($_GET['disp_id'])){
+								include 'pages/'.$_GET['disp_id'].'.php';
+							}else{
+								echo "<p>Τί θα κάνουμε σήμερα;</p><br>";
+							}
+						?>
+						
+						
+					</article>
+
+
+				</main>
+			</div>
+			<div class="wrapper row5">
+			  <footer id="footer" class="clear"> 
+				<div class="one_quarter first">
+					<h6 class="title">Test 1</h6>
+				</div>
+				<div class="one_quarter">
+					<h6 class="title">Test 2</h6>
+				</div>
+				<div class="one_quarter">
+					<h6 class="title">Test 3</h6>
+				</div>
+			  </footer>
+		</div>
+		<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+		</div>
+	</body>
+</html>
---
+++ pages/dbconnect.php
@@ -0,0 +1,16 @@
+<?php 
+	//DATABASE CONNECTION
+	$dbserver 		= "localhost";
+	$dbusername 	= "root";
+	$dbpassword 	= "";
+	$db 			= "paragelator";
+	
+	//CREATE CONNECTION
+	$conn = new mysqli($dbserver, $dbusername, $dbpassword, $db);
+	
+	//CHECK CONNECTION
+	if ($conn->connect_error)
+	{
+		die("Connection failed: ".$conn->connect_error);
+	}
+?>
